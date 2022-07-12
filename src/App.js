d523bb43ee16a92a4276587ba645ee30fdf2207c
hwanyb <mnn9502@naver.com>
2022-07-12 23:30:18 +0900

    Feat: 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ArtSchool from './pages/ArtSchool';
+import Food from './pages/Food';
+import Home from './pages/Home';
+import Introduce from './pages/Introduce';
+import Notice from './pages/Notice';
 import GlobalStyle from './style/GlobalStyle';
 
 function App() {
   return (
     <>
       <GlobalStyle/>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/introduce' element={<Introduce />} />
+          <Route path='/food' element={<Food />} />
+          <Route path='/artschool' element={<ArtSchool />} />
+          <Route path='/notice' element={<Notice />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
