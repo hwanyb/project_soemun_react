05dba0a3a0dea5248987d9409c85bc327356529d
hwanyb <mnn9502@naver.com>
2022-07-20 16:50:18 +0900

    Design: Food 페이지 추가

--- src/pages/Food.jsx
+++ src/pages/Food.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import Title from "../components/common/Title";
+
+const Base = styled.div`
+`;
 
 export default function Food() {
   return (
-    <div>Food</div>
-  )
+    <Base>
+      <Title>먹거리</Title>
+    </Base>
+  );
 }
