a69e0adfd3126f3561b80b49d8654702df0939d9
hwanyb <mnn9502@naver.com>
2022-07-22 11:00:18 +0900

    Design: LNB NavList>li width 수정

--- src/components/common/LNB.jsx
+++ src/components/common/LNB.jsx
@@ -1,42 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import styled, { css } from 'styled-components';
+import React, { useEffect, useState } from "react";
+import styled, { css } from "styled-components";
 
 const Base = styled.div`
-    background-color: #fff;
-    width: 100%;
-    height: 50px;
-    box-shadow: 1px 1px 5px #08143555;;
-    padding: 0 200px;
-    position: sticky;
-    top: 70px;
-    z-index: 9999;
+  background-color: #fff;
+  width: 100%;
+  height: 50px;
+  box-shadow: 1px 1px 5px #08143555;
+  padding: 0 200px;
+  position: sticky;
+  top: 70px;
+  z-index: 9999;
 `;
 const NavList = styled.ul`
-    display: flex;
-    justify-content: space-between;
-    line-height: 50px;
-    &>li{
-    max-width: 200px;
+  display: flex;
+  justify-content: space-between;
+  line-height: 50px;
+  & > li {
+    width: 250px;
     font-size: 18px;
-    font-family: 'PyeongChangPeace-Light';
+    font-family: "PyeongChangPeace-Light";
     cursor: pointer;
     transition: all 0.2s ease-in-out;
-    &:hover { 
-        color: #FF9900;
+    &:hover {
+      color: #ff9900;
     }
-    }
+  }
 `;
 
 export default function LNB({ children, location, setLocation }) {
-    const onClick = (e) => {
-        setLocation(e.target.id);
-      }
-      useEffect(() => {
-        console.log(location);
-      }, [location])
+  const onClick = (e) => {
+    setLocation(e.target.id);
+  };
+  useEffect(() => {
+    console.log(location);
+  }, [location]);
   return (
     <Base>
-        <NavList onClick={(e) => onClick(e)}>{ children }</NavList>
+      <NavList onClick={(e) => onClick(e)}>{children}</NavList>
     </Base>
-  )
+  );
 }
