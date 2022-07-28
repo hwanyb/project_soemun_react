530bcbc0decada4c823a664f3f19285b02dff729
hwanyb <mnn9502@naver.com>
2022-07-28 15:00:18 +0900

    Design: Header 반응형 수정

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-import { PyeongBold, PyeongLight } from '../../style/Common';
+import { PyeongBold, PyeongLight } from "../../style/Common";
 
 const Base = styled.header`
   display: flex;
@@ -15,43 +15,51 @@ const Base = styled.header`
   top: 0;
   padding: 0 200px;
   align-items: center;
-  box-shadow: 1px 1px 5px #0814352f;;
+  box-shadow: 1px 1px 5px #0814352f;
   @media screen and (max-width: 1600px) {
     padding: 0 150px;
-    
   }
   @media screen and (max-width: 1200px) {
     padding: 0 100px;
-    
   }
   @media screen and (max-width: 900px) {
     padding: 10px 50px;
     flex-direction: column;
   }
+  @media screen and (max-width: 600px) {
+    padding: 10px 20px;
+  }
 `;
 const StyledLink = styled(Link)`
-    text-decoration: none;
-    box-sizing: border-box;
+  text-decoration: none;
+  box-sizing: border-box;
+  margin-left: 30px;
+  &:first-child {
+    margin-left: 0;
+  }
 `;
 const GNB = styled.nav`
-    display: flex;
+  display: flex;
+  @media screen and (max-width: 900px) {
+    width: 100%;
+    justify-content: space-between;
+  }
 `;
 const LogoTitle = styled(PyeongBold)`
-    color: #081435;
-    font-size: 30px;
-    @media screen and (max-width: 900px) {
+  color: #081435;
+  font-size: 30px;
+  @media screen and (max-width: 900px) {
     font-size: 24px;
   }
 `;
 const NavItem = styled(PyeongLight)`
-    color: #081435;
-    font-size: 18px;
-    margin-left: 30px;
-    transition: all 0.2s ease-in-out;
-    &:hover{
-        color: #FF9900;
-    }
-    @media screen and (max-width: 900px) {
+  color: #081435;
+  font-size: 18px;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    color: #ff9900;
+  }
+  @media screen and (max-width: 900px) {
     font-size: 14px;
   }
 `;
@@ -59,9 +67,9 @@ const NavItem = styled(PyeongLight)`
 export default function Header() {
   return (
     <Base>
-        <StyledLink to="/">
-          <LogoTitle>서문시장 야시장</LogoTitle>
-        </StyledLink>
+      <StyledLink to="/">
+        <LogoTitle>서문시장 야시장</LogoTitle>
+      </StyledLink>
       <GNB>
         <StyledLink to="/introduce">
           <NavItem>소개</NavItem>
