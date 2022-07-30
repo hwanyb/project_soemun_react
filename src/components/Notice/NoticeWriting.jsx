eeff05c31db63832d824ce24f31a5aeeaef4005f
hwanyb <mnn9502@naver.com>
2022-07-30 16:10:18 +0900

    Design: NoticeWriting 컴포넌트 css 수정

--- src/components/Notice/NoticeWriting.jsx
+++ src/components/Notice/NoticeWriting.jsx
@@ -13,7 +13,7 @@ const Label = styled(Noto300)`
 `;
 const TitleInput = styled.input`
   color: #081435;
-  font-size: 16px;
+  font-size: 14px;
   border: 1px solid #ffc806;
   width: 100%;
   padding: 5px 20px;
@@ -22,10 +22,10 @@ const TitleInput = styled.input`
 `;
 const BodyInput = styled.textarea`
   color: #081435;
-  font-size: 16px;
+  font-size: 14px;
   border: 1px solid #ffc806;
   width: 100%;
-  min-height: 200px;
+  min-height: 400px;
   padding: 5px 20px;
   border-radius: 15px;
   margin-bottom: 50px;
@@ -74,25 +74,24 @@ export default function NoticeWriting({ setMainView, noticeList }) {
     });
     await localStorage.setItem("noticeArr", JSON.stringify(newNoticeList));
     setMainView("list");
-
   };
   return (
-      <NoticeForm onSubmit={(e) => onSubmit(e)}>
-        <Label>제목</Label>
-        <TitleInput
-          name="title"
-          type="text"
-          placeholder="제목을 입력해 주세요."
-          onChange={onChange}
-        />
-        <Label>내용</Label>
-        <BodyInput onChange={(e) => onChange(e)} name="body" />
-        <BtnWrapper>
-          <FormBtn type="submit" disabled={title.length < 1 || body.length < 1}>
-            저장
-          </FormBtn>
-          <FormBtn onClick={onCancleClick}>취소</FormBtn>
-        </BtnWrapper>
-      </NoticeForm>
+    <NoticeForm onSubmit={(e) => onSubmit(e)}>
+      <Label>제목</Label>
+      <TitleInput
+        name="title"
+        type="text"
+        placeholder="제목을 입력해 주세요."
+        onChange={onChange}
+      />
+      <Label>내용</Label>
+      <BodyInput onChange={(e) => onChange(e)} name="body" />
+      <BtnWrapper>
+        <FormBtn type="submit" disabled={title.length < 1 || body.length < 1}>
+          저장
+        </FormBtn>
+        <FormBtn onClick={onCancleClick}>취소</FormBtn>
+      </BtnWrapper>
+    </NoticeForm>
   );
 }
